f781f9e85d6518ba63d2c8eae5bdd96aa24fbbcc
Thyago <thiagogg020676@gmail.com>
2024-09-02 22:46:30 -0300

    ....

--- database/database.js
+++ database/database.js
@@ -1,8 +1,17 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('cardapio', 'root', '98654321Thiago', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
+const database = process.env.DATABASE;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD; // Verifique se este é o nome correto
+const port = process.env.DB_PORT; // Porta padrão para MySQL
+const host = process.env.DB_HOST
+
+const connection = new Sequelize(database, username, password, {
+  host: host,
+  port: port,
+  dialect: 'mysql',
+  logging: false,
+});
+
 
 module.exports = connection;
